704e7a469c9c3a952a7126fae89701a27a7beab2
Xasanboy0916 <hasanboymukhtorv@gmail.com>
2025-11-14 16:40:17 +0500

    feat: added operation if-else

--- javascript/if-else.js
+++ javascript/if-else.js
@@ -1,8 +1,36 @@
-const number = 7;
-if (number > 0 && number < 5) {
-  console.log("Berilgan son musbat");
-} else {
-  console.log("Berilgan son manfiy");
+// const number = 7;
+// if (number > 0 && number < 5) {
+//   console.log("Berilgan son musbat");
+// } else {
+//   console.log("Berilgan son manfiy");
+// }
+
+// console.log("if blockidan keyin code qatori");
+// const score = 34;
+// if (score >= 56 && score <= 69) {
+//   console.log("Sizning bahoingiz 3 baho");
+// } else if (score >= 70 && score <= 85) {
+//   console.log("Sizning bahoingiz 4 baho");
+// } else if (score >= 86 && score <= 100) {
+//   console.log("Sizning bahoingiz 5 baho");
+// } else (score >= 0 && score <= 55) {
+//   console.log("Siz imtihondan o'taolmadingiz");
+// }
+
+let productPrice = 100;
+const isMember = true;
+const hasCoupon = false;
+
+if (isMember) {
+  console.log("10% chegirma olasiz");
+  productPrice = productPrice - productPrice * 0.9;
+}
+if (hasCoupon) {
+  console.log("5% chegirma olasiz");
+  productPrice = productPrice - productPrice * 0.95;
 }
 
-console.log("if blockidan keyin code qatori");
+if (productPrice < 50) {
+  console.log("5% chegirma");
+  productPrice = productPrice - productPrice * 0.95;
+}
